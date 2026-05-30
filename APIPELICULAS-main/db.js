986e7dc8cda6b9d46c31aef48967599fa1d7ef7e
Marckitos610e <ma.paz24@info.uas.edu.mx>
2026-05-29 21:46:08 -0700

    feat: migración completa a PostgreSQL en Render

--- APIPELICULAS-main/db.js
+++ APIPELICULAS-main/db.js
@@ -1,10 +1,14 @@
 const { Sequelize } = require('sequelize');
-const sqlite3 = require('sqlite3-offline'); // <--- Importamos la versión offline
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-  dialectModule: sqlite3, // <--- ¡Esta es la clave para Render!
+// Pega aquí adentro el enlace largo que copiaste de Render
+const sequelize = new Sequelize('postgresql://peliculas_db_6hua_user:EKxVD9WopNtrYHIpJMmmxYSgZ9EG11FH@dpg-d8d6m5740ujc73cbs15g-a.oregon-postgres.render.com/peliculas_db_6hua', {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Requerido obligatoriamente por Render
+    }
+  },
   logging: false
 });
 
