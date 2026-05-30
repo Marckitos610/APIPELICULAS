885820160e97376a4bb1c7e758d48cec9b526f20
Marckitos610e <ma.paz24@info.uas.edu.mx>
2026-05-29 21:39:51 -0700

    fix: usar dialectModule con sqlite3-offline en db

--- APIPELICULAS-main/db.js
+++ APIPELICULAS-main/db.js
@@ -1,7 +1,11 @@
 const { Sequelize } = require('sequelize');
+const sqlite3 = require('sqlite3-offline'); // <--- Importamos la versión offline
+
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: './database.sqlite',
+  dialectModule: sqlite3, // <--- ¡Esta es la clave para Render!
   logging: false
 });
+
 module.exports = sequelize;
